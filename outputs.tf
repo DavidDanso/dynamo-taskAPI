796649775fee9aa724aad50ca6c65a2aef72a859
DavidDanso <desiingerboy@icloud.com>
2026-02-28 02:15:46 +0000

    feat: Add API base URL, Lambda function name, and DynamoDB table name outputs, and remove unused VPC ID and SSH connection string outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,14 @@
-output "demo_t" {
-  description = ""
-  value       = aws_vpc.main.id
+output "api_base_url" {
+  description = "Base URL for the tasks API"
+  value       = "https://${aws_api_gateway_rest_api.tasks_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.dev.stage_name}/tasks"
 }
 
-output "ssh_connection_string" {
-  value = ""
+output "lambda_function_name" {
+  description = "Name of the Lambda function"
+  value       = aws_lambda_function.tasks_function.function_name
+}
+
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB table"
+  value       = aws_dynamodb_table.tasks.name
 }
